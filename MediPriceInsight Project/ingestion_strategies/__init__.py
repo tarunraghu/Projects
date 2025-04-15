f6b6381f147dc43ad8f5e7645f19e6e57997ab0a
tarunraghu <rgh.tarun@gmail.com>
2025-04-14 21:18:06 -0500

    revert to working version (type 1 and type 2 working version)

--- MediPriceInsight Project/ingestion_strategies/__init__.py
+++ MediPriceInsight Project/ingestion_strategies/__init__.py
@@ -1,5 +1,4 @@
 from .base_strategy import BaseIngestionStrategy
 from .type1_strategy import Type1IngestionStrategy
-from .type3_strategy import Type3IngestionStrategy
 
-__all__ = ['BaseIngestionStrategy', 'Type1IngestionStrategy', 'Type3IngestionStrategy'] 
+__all__ = ['BaseIngestionStrategy', 'Type1IngestionStrategy'] 
