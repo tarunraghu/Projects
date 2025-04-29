32abb885850ed6a0fcc9f8a299c53fde3eeca9a2
tarunraghu <rgh.tarun@gmail.com>
2025-04-29 10:34:11 -0500

    Delete MediPriceInsight Project/Front End Project directory

--- MediPriceInsight Project/Front End Project/config.py
+++
@@ -1,8 +0,0 @@
-# PostgreSQL Database Configuration
-DB_CONFIG = {
-    'host': 'localhost',
-    'database': 'healthcarepoc',
-    'user': 'postgres',
-    'password': 'Consis10C!',
-    'port': '5432'
-} 
